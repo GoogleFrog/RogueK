29f0e90c699431c71a9d494f1d23e47e509b9791
GoogleFrog <googl3frog@gmail.com>
2025-01-07 12:36:35 +1100

    Base turret replacer.

---
+++ modinfo.lua
@@ -0,0 +1,13 @@
+return {
+	name='Rogue-K',
+	description='Rogue-K',
+	shortname='RK',
+	version='$VERSION',
+	mutator='1',
+	game='Zero-K',
+	shortGame='ZK',
+	modtype=1,
+	depend = {
+		'Zero-K $VERSION',
+	},
+}
